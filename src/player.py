a24e7d79a6d1da45a1795d1c3501015d7fd9abdb
Lucas <lucaskreuch77@gmail.com>
2023-05-23 17:37:44 -0300

    player movement

--- src/player.py
+++ src/player.py
@@ -13,6 +13,10 @@ class Player(Object):
     
         self.player_img = image
         self.animation = animation
+        self.direction = pygame.math.Vector2()
+
+        self.speed = 5
+        self.is_running = False
 
         self.image = self.player_img.image
         self.rect = self.image.get_rect()
@@ -23,18 +27,43 @@ class Player(Object):
 
         self.animation.select(PLAYER_IDLE)
 
-    def movement(self):
+    def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
-            pass
-        if keys[pygame.K_s]:
-            pass
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else: 
+            self.direction.y = 0
+
         if keys[pygame.K_d]:
-            pass
-        if keys[pygame.K_a]:
-            pass
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self):
+        self.is_running = False
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+            self.is_running = True
+        
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+    def animation_control(self):
+        self.animation.select(PLAYER_IDLE)
+        if self.is_running:
+            self.animation.select(PLAYER_RUN)
+
         
     def update(self):
-        self.movement()
+        self.input()
+        self.move()
+        self.animation_control()
+
         self.image = self.animation.update_animation()
+
+        print(self.is_running)
